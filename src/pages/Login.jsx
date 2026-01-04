35f37f85149280969df79ca1ec563afbc842db09
notgodfather <ankitranjan10may@gmail.com>
2026-01-04 17:15:04 +0530

    react import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 // frontend/src/pages/Login.jsx - FIXED 404 + PRODUCTION READY
+import React from 'react';
 import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import api from '../services/api';
